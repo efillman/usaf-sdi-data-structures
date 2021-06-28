aa5bf2b2c8cc60cb129a85ad0f635f5d80d11985
Evan Fillman <efillman@gmail.com>
2021-06-28 17:47:55 -0400

    wayst adds

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,51 @@
 var Queue = function() {
-  var someInstance = {};
+  var queueData = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  queueData.queueStorage = {};
+  queueData.queueFrontPointer = 0;
+  queueData.queueBackPointer = -1;
 
-  // Implement the methods below
-
-  someInstance.enqueue = function(value) {
+  //`enqueue(element)` - Adds an element to the back of the queue
+  queueData.enqueue = function(value) {
+    queueData.queueBackPointer++
+    queueData.queueStorage[`${queueData.queueBackPointer}`] = value;
   };
 
-  someInstance.dequeue = function() {
+  //`dequeue()` - Remove and return the element at the front of the queue
+  queueData.dequeue = function() {
+    if (queueData.queueBackPointer > -1) {
+      let dequeuePointer = queueData.queueFrontPointer;
+      let dequeueValue =  queueData.queueStorage[dequeuePointer]
+      queueData.queueFrontPointer++;
+      delete queueData.queueStorage[dequeuePointer];
+      return dequeueValue;
+    }
   };
 
-  someInstance.size = function() {
+  //`size()` - Return the number of items in the queue
+  queueData.size = function() {
+    return queueData.queueBackPointer + 1;
   };
 
-  return someInstance;
+  //`front()` - Return the element at the front of the queue
+  queueData.front = function() {
+    return queueData.queueStorage[`${queueData.queueFrontPointer}`];
+  };
+
+  //`isEmpty()` - Returns true or false whether the queue has elements
+  queueData.isEmpty = function() {
+    if (queueData.queueBackPointer < 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  //`printQueue()` - Prints all the elements of the queue
+  queueData.printQueue = function() {
+  };
+
+  //
+  return queueData;
 };
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,59 @@
 var Stack = function() {
-  var someInstance = {};
+  var stackData = {
+  };
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  stackData.stackStorage  = {}
+  stackData.stackPointer = -1
 
-  // Implement the methods below
-  someInstance.push = function(value) {
+  //`push(element)` - Add an element to the top of the stack
+  stackData.push = function(value) {
+    ++stackData.stackPointer;
+    stackData.stackStorage[`${stackData.stackPointer}`] = value;
   };
 
-  someInstance.pop = function() {
+  //`pop()` - Remove and return the element on the top of the stack
+  stackData.pop = function() {
+    if (stackData.stackPointer > -1) {
+    let popPointer = stackData.stackPointer;
+    let popValue =  stackData.stackStorage[popPointer]
+    stackData.stackPointer--;
+    delete stackData.stackStorage[popPointer];
+    return popValue;
+    }
   };
 
-  someInstance.size = function() {
+  //`size()` - Return the number of items on the stack
+  stackData.size = function() {
+    return stackData.stackPointer+1;
   };
 
-  return someInstance;
+  //`peek()` - Return the top element on the stack, but it won't remove it (the way `pop()` does)
+  stackData.peek = function() {
+    if (stackData.stackPointer > -1) {
+      let popPointer = stackData.stackPointer;
+      let popValue =  stackData.stackStorage[`${popPointer}`]
+      return popValue;
+    }
+  };
+
+  //`isEmpty()` - Returns whether or not the Stack is empty
+  stackData.isEmpty = function() {
+    if (stackData.stackPointer === -1) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  //`printStack()` - Prints all the elements of a queue
+  stackData.printStack = function() {
+    if (stackData.stackPointer > -1) {
+    let result = '';
+      for (let i = 0; i <= stackData.stackPointer; i++) {
+        result = result + stackData.stackStorage[`${i} `]
+    }
+  }
+  };
+  return stackData;
 };

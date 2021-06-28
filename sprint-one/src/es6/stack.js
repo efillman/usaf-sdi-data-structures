39508e721882326ed57ecd326fc693e79d4f79ba
bryson crandall <bryson.crandall@yahoo.com>
2021-06-28 13:41:59 -0700

    stack es6 implementation

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,51 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.items = {};
+    this.top = -1;
+  }
+  push(element) {
+    //increment top pointer
+    //add key:value to items object at that pointer
+    this.top++
+    this.items[`${this.top}`] = element
+  }
+  pop() {
+    //take the item at from the top pointer return it
+    //remove the item from the stack at the top pointer
+    //decrement top pointer
+    if (this.top !== -1) {
+      const temp = this.items[`${this.top}`]
+      delete this.items[`${this.top}`]
+      this.top = this.top - 1;
+      return temp;
+    }
+  };
+  size() {
+    if (this.top === -1) { return 0 }
+    else { return this.top + 1 }
   }
 
+  peek() {
+    return this.items[`${this.top}`]
+  }
+  //Prints all the elements of a queue
+  printStack() {
+    let result = ''
+    for(let i = 0; i <= this.top; i++){
+      result += this.items[`${i}`]
+      if(i !== this.top){result += ' '}
+    }
+    return result
+  }
+
+  isEmpty() {
+    if (this.top > -1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
+
+
+
